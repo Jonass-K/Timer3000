e23cdde679a02c9f039a4cb3188faecb6106c83b
Jonass-K <joonaskaiser@gmail.com>
2021-03-22 18:25:09 +0100

    Initial Commit

---
+++ Shared/Timer3000App.swift
@@ -0,0 +1,17 @@
+//
+//  Timer3000App.swift
+//  Shared
+//
+//  Created by Jonas Kaiser on 22.03.21.
+//
+
+import SwiftUI
+
+@main
+struct Timer3000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
